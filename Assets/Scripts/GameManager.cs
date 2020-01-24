7c30c604ab8977508c495023e77331e480f255fa
Danny <danielisidrocustodioduran@gmail.com>
2020-01-23 18:23:14 -0600

    Example of multiplayer

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Room", new RoomOptions {MaxPlayers=3}, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("On joined room");
+        PhotonNetwork.Instantiate("Player", new Vector2(Random.Range(-4, 4), Random.Range(-4, 4)), Quaternion.identity);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
+        
+        _sr.color = new Color(
+            (float)Random.Range(0, 255)/255,
+            (float)Random.Range(0, 255)/255,
+            (float)Random.Range(0, 255)/255,
+            1f
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 inputMovement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0);
+        transform.position += inputMovement.normalized * Time.deltaTime * 5.0f;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _rb.AddForce(transform.up * 1.0f, ForceMode2D.Impulse);
+        }
+    }
+}
